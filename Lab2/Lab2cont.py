3bd2c4ae079a15002e4b63ff6ba7fc8965a826d8
Akshay Chandiramani <s1558717@banshee.inf.ed.ac.uk>
2018-10-18 08:11:58 +0100

    Lab2 complete

--- Lab2/Lab2cont.py
+++ Lab2/Lab2cont.py
@@ -1,24 +1,18 @@
 from nltk.corpus import stopwords
 from stemming.porter2 import stem
 import nltk
-file_content = open("collections/sample.txt").read()
-tokens = nltk.word_tokenize(file_content)
-tokens = [token.lower() for token in tokens if token.isalpha() and token not in stopwords.words('english')]
-#tokens = [stem(token) for token in tokens]
-tokens = [token for token in tokens if token != "id" and token != "text"]
-output = set(tokens)
-tokens = list(output)
 
-with open("collections/sample.txt") as fp, open("preprocess.txt", "w") as fr:
+with open("collections/trec.sample.txt") as fp, open("preprocess.txt", "w") as fr:
     #Get all the lines in the document
-    lis = []
-    '''for line in fp:
-        if line[0] != "I" and line[0] != "T":
-            lis.append(line + " " + fp.next())'''
-    for line in fp:
-        if line[0] != "I":
-            lis.append(line)
+    lis = {}
 
+    #If we come to the ID line, store
+    for line in fp:
+        if line [0] == "I":
+            headtext = ""
+            for i in range(2):
+                headtext = headtext + fp.next() + " "
+            lis.update({line[4:] : headtext})
     """The dictionary here has the word as keys, and another dictionary as the values
     Which have the document IDs as keys and a list of occurences for that word in
     the document as value"""
@@ -26,12 +20,16 @@ with open("collections/sample.txt") as fp, open("preprocess.txt", "w") as fr:
     #For each of the text lines (documents)
     for ele in lis:
         #Break out the words in the line, convert to lowercase and preprocess
-
-        linewo = nltk.word_tokenize(ele)
+        linewo = nltk.word_tokenize(lis[ele])
         linewo = [wo for wo in linewo if wo!="TEXT" and wo != "HEADLINE"]
-        linewo = [wo.lower() for wo in linewo if wo.isalpha()]
+        linewo = [wo.lower() for wo in linewo if wo.isalnum()]
+        #and token not in stopwords.words('english')
+        #tokens = [stem(token) for token in tokens]
+
         #Here we get the respective document index
-        doc_index = (lis.index(ele) + 1)
+        doc_index = ele
+        #print (doc_index)
+
         #If a word is in a document, add all of its positions to the dictionary
         for wo in linewo:
             if wo not in dict:
@@ -52,7 +50,3 @@ with open("collections/sample.txt") as fp, open("preprocess.txt", "w") as fr:
                 fr.write(",".join([str(i) for i in dict[x][y]]))
                 fr.write("\n")
             fr.write("\n")
-
-f = open("output.txt", "a")
-f.write(", ".join(tokens))
-f.close()
